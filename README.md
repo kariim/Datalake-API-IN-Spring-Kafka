85b91aa374cf6aaa563b0fc690a8825db6ae465a
Karim <kariim.oubaha@gmail.com>
2017-12-22 00:25:27 +0100

    Simple Kafka Consumer implementation

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# API-IN
+Local install instructions On windows
+-------------------------------------
+
+Start Zookeeper
+---------------
+ => Open a terminal
+ => type command : zkserver
+
+
+Start Kafka
+-----------
+ => Open a new command prompt in the location D:\Software\kafka_2.10-0.10.1.0\bin\windows
+ => type command : kafka-server-start.bat ..\..\config\server.properties
+
+
+Create Kafka topic
+------------------
+ => Open a new command prompt in the location D:\Software\kafka_2.10-0.10.1.0\bin\windows
+ => kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic topic_name
+
+
+Create Kafka Producer
+---------------------
+ => Open a new command prompt in the location D:\Software\kafka_2.10-0.10.1.0\bin\windows
+ => kafka-console-producer.bat --broker-list localhost:9092 --topic topic_name
+
+
+Create Kafka Consumer
+---------------------
+ => Open a new command prompt in the location D:\Software\kafka_2.10-0.10.1.0\bin\windows
+ => kafka-console-consumer.bat --zookeeper localhost:2181 --topic topic_name
---
+++ src/main/resources/initialization/initialize_kafka.bat
@@ -0,0 +1,14 @@
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic  defaut-metrics
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi-test2
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi1-nucleaire-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi1-o2-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi1-plateaurenouveau-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi1-plateausdin-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi1-plateausint-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi1-ris-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi1-thit-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi2-dsc-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi2-fgai-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi2-mat-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi2-mmd-surveillance
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic osi2-wrh-surveillance
